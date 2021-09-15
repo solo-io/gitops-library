c0eb67df7884137427f07da3707eb8cde66c6877
Alex <alexbrucely@gmail.com>
2021-09-15 09:12:51 -0700

    update wait-for script to understand contexts

--- tools/wait-for-rollout.sh
+++ tools/wait-for-rollout.sh
@@ -7,9 +7,10 @@ rollout_type=$1
 rollout_name=$2
 namespace=$3
 period=$4
+context=$5
 
 while [ "$OUTPUT" -ne 1 ]; do
-  OUTPUT=`kubectl get ${rollout_type}/${rollout_name} -n ${namespace} 2>/dev/null | grep -c ${rollout_name}`;
+  OUTPUT=`kubectl get ${rollout_type}/${rollout_name} -n ${namespace} --context ${context} 2>/dev/null | grep -c ${rollout_name}`;
   seconds=$((seconds+${period}))
   printf "Waiting %s seconds for ${rollout_type} ${rollout_name} to come up.\n" "${seconds}"
   sleep ${period}
