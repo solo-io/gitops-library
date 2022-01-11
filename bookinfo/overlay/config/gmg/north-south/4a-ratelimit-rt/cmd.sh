d042125b1d4f21ba6c94ab541945eb3127f9e642
Alex <alexbrucely@gmail.com>
2022-01-11 10:39:46 -0800

    testing new structure

---
+++ bookinfo/overlay/config/gmg/north-south/4a-ratelimit-rt/cmd.sh
@@ -0,0 +1,3 @@
+for i in {1..5}; do curl -I -H "x-type: a" -H "x-number: one" -sk localhost:8080/productpage;echo ''; done
+
+for i in {1..5}; do curl -I -H "x-type: a" -H "x-number: one" -sk localhost:8081/productpage;echo ''; done
