a28fee557bf3b1c5da29497bb204380207387914
Alex <alexbrucely@gmail.com>
2023-12-07 16:30:05 -0800

    update argocd to 2.8.0

--- argocd/deploy/install-argocd.sh
+++ argocd/deploy/install-argocd.sh
@@ -3,18 +3,6 @@
 INSTALL_TYPE=$1 # default/insecure/insecure-rootpath
 CONTEXT=$2
 
-### If the CONTEXT is not specified, simply use the default context.
-if [[ ${CONTEXT} == "" ]]
-then
-  CONTEXT=`kubectl config current-context`
-  if [[ ${CONTEXT} == "" ]]
-  then
-    echo "You do not have a curent kubernetes cluster.  Please create one."
-    exit 1
-  fi
-  echo "No context specified. Using default context of ${CONTEXT}"
-fi
-
 # argo install type
 if [[ ${INSTALL_TYPE} == "" ]]
   then
@@ -36,3 +24,21 @@ kubectl --context ${CONTEXT} -n argocd patch secret argocd-secret \
     "admin.password": "$2a$10$79yaoOg9dL5MO8pn8hGqtO4xQDejSEVNWAGQR268JHLdrCw6UCYmy",
     "admin.passwordMtime": "'$(date +%FT%T%Z)'"
   }}'
+
+# create argo app-of-apps project
+kubectl apply --context ${CONTEXT} -f- <<EOF
+apiVersion: argoproj.io/v1alpha1
+kind: AppProject
+metadata:
+  name: app-of-apps
+  namespace: argocd
+spec:
+  clusterResourceWhitelist:
+  - group: '*'
+    kind: '*'
+  destinations:
+  - namespace: '*'
+    server: '*'
+  sourceRepos:
+  - '*'
+EOF
